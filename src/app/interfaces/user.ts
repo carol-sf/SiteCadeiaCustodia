86d16d31a02a9c6c6a356d3a0abb5a96ecb57ecd
davitostes <davisiqueira591@gmail.com>
2024-01-03 09:12:00 -0300

    feat: adicionando firebase e userService

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,19 @@
+import { AddPrefixToKeys } from "firebase/firestore";
+
+export interface User extends AddPrefixToKeys<string, any> {
+    id: string,
+    matricula: string,
+    nome: string,
+    email: string,
+    senha: string,
+    posto: string,
+    ativo: boolean,
+    tipo: UserType
+}
+
+export enum UserType {
+    Perito = 0,
+    Admin = 1,
+    SuperAdmin = 2,
+    Operador = 3
+}
